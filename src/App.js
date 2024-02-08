452354bbf90a5caa89bc3f3469ef8c4e78daaec7
Sudeep-K <pong32btl@gmail.com>
2024-02-08 23:05:10 +0545

    yeeyeeasscommitnigga

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import PostsList from './components/PostsList ';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [postField, setPostField] = useState({
+    'title': '',
+    'body': ''
+  });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, [])
+
+  useEffect(() => {
+    setLoading(false);
+  }, [posts])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setPosts(current => [...current, {
+      'id': posts.length + 1,
+      'title': postField.title,
+      'body': postField.body,
+    }])
+
+    setPostField({
+      'title': '',
+      'body': ''
+    })
+  }
+
+  const handleDelete = (id) => {
+    setPosts(current =>
+      current.filter(
+        post => {
+          return post.id !== id;
+        }
+      )
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col gap-4 max-w-5xl mx-auto">
+      {loading ? <h1>loading...</h1> : <PostsList posts={posts} postField={postField} setPostField={setPostField} handleSubmit={handleSubmit} handleDelete={handleDelete} />}
     </div>
   );
 }
